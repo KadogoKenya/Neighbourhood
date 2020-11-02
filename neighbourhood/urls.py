b8d935a840dd5ba36f4038bf0051d0cbf699ad55
Kate <you@example.com>
2020-11-02 08:53:36 +0300

    url for details posting redirected

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,6 +1,9 @@
 from . import views 
 from django.urls import path
 from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -8,4 +11,5 @@ urlpatterns = [
     # path('tutorial/new/', TutorialCreateView.as_view(), name='tutorial-create'),
     # path('', TutorialListView.as_view(), name='index'),
     path('', views.index, name='index'),
+    path('add_neighbourhood/', views.add_neighbourhood, name='add_neighbourhood'),
 ]
