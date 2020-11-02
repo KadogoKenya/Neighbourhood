5826ab328b72d8ff8cb9fe0ee002974f56aa0465
Kate <you@example.com>
2020-11-02 05:55:16 +0300

    creating views

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404
 from users.models import Profile
 from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood, Post, Business
 
 # Create your views here.
 @login_required(login_url='/login/')
